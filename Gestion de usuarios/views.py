fd0fee85224a3c86b2c0527e9d9bf99050fbb11f
Sofia <sofia.rodriguez193667@alumnos.info.unlp.edu.ar>
2024-04-29 00:22:44 -0300

    Agrege carpeta de gestion de usuarios y un txt general de sugerencias

---
+++ Gestion de usuarios/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+
+#funcion para registrar un usuario 
+def register(request):
+    data = {
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        user_creation_form = CustomUserCreationForm(data=request.POST)
+
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+
+            user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('home')
+        else:
+            data['form'] = user_creation_form
+
+    return render(request, 'registration/register.html', data)

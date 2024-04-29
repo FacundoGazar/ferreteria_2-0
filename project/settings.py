8d98202c455695d444813862f65215df2b908650
Facundo <facundobarciabarate@gmail.com>
2024-04-29 16:22:05 -0300

    Funcionalidad completa de recuperar contrasena

--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,11 @@ STATICFILES_FINDERS = [
 ]
 
 SASS_PROCESSOR_ROOT = STATIC_URL
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ingenieriagrupo45@gmail.com'
+EMAIL_HOST_PASSWORD = 'gntn pcro dpiz komp'

401618db6ad4ed31b883eff8326ed11cfd5f268b
Sofia <sofiac2507@gmail.com>
2024-05-01 19:49:55 -0300

    arregle agregar sucursal y listar sucursales de nuevo

--- gestion_de_sucursales/models.py
+++ gestion_de_sucursales/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Sucursal(models.Model):
     nombre = models.CharField(max_length=200, primary_key=True)
-    ciudad = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=200, null=False)
 
     def __str__(self):
         return self.nombre

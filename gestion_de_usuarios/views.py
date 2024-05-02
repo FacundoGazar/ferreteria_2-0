f6bbd2ed078300f756afd2651ea31d279dcf308c
Sofi <sofiarodrigueztec@gmial.com>
2024-05-02 16:47:24 -0300

    Funciona agregar usuario

--- gestion_de_usuarios/views.py
+++ gestion_de_usuarios/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from gestion_de_usuarios.models import PerfilCliente
-import tkinter as tk
-from tkinter import messagebox
+from django.contrib import messages
 
 def registrar(request):
     return render(request, 'gestion_de_usuarios/registrar.html')
@@ -21,21 +20,23 @@ def register(request):
         edad = int(request.POST.get('edad')) 
 
         if User.objects.filter(username=usuario).exists():
-            return render(request, 'gestion_de_usuarios/registrar.html', {'error_message': 'El nombre de usuario ya está en uso'})
+            messages.success(request, "Se registro el usuario")
+            return render(request, 'gestion_de_usuarios/registrar.html')
+            #return render(request, 'gestion_de_usuarios/registrar.html', {'error_message': 'El nombre de usuario ya está en uso'})
       
         # Verificar que sea mayor de dad
-        if edad >= 18:
+        if edad>= 18:
             # Crear un nuevo usuario en el sistema
             nuevo_usuario = User.objects.create_user(username=usuario, email=email, password=contrasenia, first_name=nombre, last_name=apellido)
             # Crear un perfil de cliente asociado al nuevo usuario
             perfil_cliente = PerfilCliente(user=nuevo_usuario, ciudad=ciudad, edad=edad)
             perfil_cliente.save()
-            messagebox.showinfo("Se registro el usuario")
+            messages.success(request, "Se registro el usuario")
             
             # Autenticar al usuario recién creado
-            user = authenticate(usuario=usuario,contrasenia=contrasenia)
-            login(request, usuario)
-            return redirect('homepage')
+            user = authenticate(username=usuario,password=contrasenia)
+            login(request, user)
+            return render (request, '/')
         else:
            return render(request, 'gestion_de_usuarios/registrar.html', {'error_message': 'Debe ser mayor de edad para poder registrarse'})
-
+    return render(request, "/")

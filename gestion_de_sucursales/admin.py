4e8221021f8537e91ea396b187b8312fef46ba69
Facundo <facundobarciabarate@gmail.com>
2024-05-05 18:07:24 -0300

    Permisos para acceder a la url

--- gestion_de_sucursales/admin.py
+++ gestion_de_sucursales/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Sucursal
+from .models import Sucursal, PerfilEmpleado
 
 # Register your models here.
 
 admin.site.register(Sucursal)
+admin.site.register(PerfilEmpleado)
--- gestion_de_sucursales/views.py
+++ gestion_de_sucursales/views.py
@@ -64,18 +64,21 @@ def delete_sucursal_view(request):
         messages.error(request, ("Solicitud Invalida"))
         return redirect('eliminar_sucursal')
     
+@super_user
 def gestion_de_empleados_view (request):
     empleados = PerfilEmpleado.objects.all()
     return render(request, "gestion_de_sucursales/gestion_de_empleados.html",{'empleados': empleados})
 
+@super_user
 def dar_de_baja_view(request):
     return redirect('gestion_de_empleados')
 
-
+@super_user
 def agregar_empleado_view(request):
     sucursales = Sucursal.objects.all()
     return render(request, "gestion_de_sucursales/agregar_empleado.html", {'sucursales': sucursales})
 
+@super_user
 def registrar_empleado (request):
     if request.method == 'POST':
         usuario = request.POST.get('usuario')
@@ -100,4 +103,4 @@ def registrar_empleado (request):
         perfil_empleado.save()
         messages.success(request, "Se registro el usuario")
 
-        return redirect('agregar_empleado')   
+        return redirect('agregar_empleado')

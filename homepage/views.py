530a75dd756e9e4e09e7d23b800d2cf5e2909faf
Sofia <128095398+sofiarod2002@users.noreply.github.com>
2024-05-29 22:28:28 -0300

    agrego filtros de busqueda

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-
+from gestion_de_sucursales.models import Sucursal
+from mis_productos.models import Producto
 # Create your views here.
 
 def homepage_view(request):
     return render(request, "homepage/homepage.html")
+
+def tu_vista(request):
+    sucursales = Sucursal.objects.all()
+    estados_list = ["Nuevo - Sin uso", "Usado", "Restaurado", "Para reparar", "Para piezas", "Con defectos leves"]
+    categorias_list = ["Herramientas", "Construcción", "Ferretería general", "Electricidad", "Fontanería", "Jardín"]
+
+    contexto = {
+        'sucursales': sucursales,
+        'estados_list': estados_list,
+        'categorias_list': categorias_list
+    }
+
+    return render(request, "homepage/homepage.html", contexto)
+
+def mostrar_todos_los_productos(request):
+    # Obtener todos los productos excluyendo los del usuario actual
+    if request.user.is_authenticated:
+        lista_productos = Producto.objects.exclude(cliente=request.user)
+    else:
+        lista_productos = Producto.objects.all()
+
+    # Pasar la lista de productos al contexto
+    contexto = {
+        'lista': lista_productos
+    }
+
+    # Renderizar la plantilla con el contexto
+    return render(request, "homepage/homepage.html", contexto)

10d8072d4f9eedb2e7884358cc340e4be070b386
Sofia <128095398+sofiarod2002@users.noreply.github.com>
2024-05-29 22:08:03 -0300

    funciona filtrar productos

--- busqueda_de_productos/views.py
+++ busqueda_de_productos/views.py
@@ -1,24 +1,5 @@
 from django.shortcuts import render
 from mis_productos.models import Producto
-from mis_productos.models import Producto
-from django.contrib.auth.models import User
-
-def buscar_productos2(request):
-    usuario_actual = request.user
-    if hasattr(usuario_actual, 'perfilcliente'):
-        queryset = Producto.objects.exclude(cliente=usuario_actual)
-    else: 
-        queryset = Producto.objects.all()
-
-    # Capturar el parámetro de búsqueda
-    busqueda = request.GET.get('busqueda')
-    if busqueda:
-        queryset = queryset.filter(nombre__icontains=busqueda)
-
-    context = {
-        "lista": queryset
-    }
-    return render(request, "busqueda_de_productos/buscar_productos.html", context)
 from django.db.models import Q
 
 def buscar_productos(request):
@@ -29,15 +10,15 @@ def buscar_productos(request):
         queryset = Producto.objects.all()
 
     # Capturar el parámetro de búsqueda
-    busqueda = request.GET.get('busqueda')
+    busqueda = request.GET.get('busquedas')
 
     # Filtrar por categoría
-    categorias = request.GET.getlist('categoria')
+    categorias = request.GET.getlist('categorias')
     if categorias:
         queryset = queryset.filter(categoria__in=categorias)
 
     # Filtrar por estado
-    estados = request.GET.getlist('estado')
+    estados = request.GET.getlist('estados')
     if estados:
         queryset = queryset.filter(estado__in=estados)
 

79b0614dd9c447fd7b9c860b3d78462fcb776cc5
Sofi <sofiarodrigueztec@gmial.com>
2024-05-30 14:41:25 -0300

    Corregimos cosas

--- homepage/views.py
+++ homepage/views.py
@@ -10,26 +10,18 @@ def tu_vista(request):
     sucursales = Sucursal.objects.all()
     estados_list = ["Nuevo - Sin uso", "Usado", "Restaurado", "Para reparar", "Para piezas", "Con defectos leves"]
     categorias_list = ["Herramientas", "Construcción", "Ferretería general", "Electricidad", "Fontanería", "Jardín"]
-
-    contexto = {
-        'sucursales': sucursales,
-        'estados_list': estados_list,
-        'categorias_list': categorias_list
-    }
-
-    return render(request, "homepage/homepage.html", contexto)
-
-def mostrar_todos_los_productos(request):
     # Obtener todos los productos excluyendo los del usuario actual
     if request.user.is_authenticated:
         lista_productos = Producto.objects.exclude(cliente=request.user)
     else:
         lista_productos = Producto.objects.all()
 
-    # Pasar la lista de productos al contexto
     contexto = {
-        'lista': lista_productos
+        'sucursales': sucursales,
+        'estados_list': estados_list,
+        'categorias_list': categorias_list,
+        'lista_productos': lista_productos
     }
 
-    # Renderizar la plantilla con el contexto
     return render(request, "homepage/homepage.html", contexto)
+

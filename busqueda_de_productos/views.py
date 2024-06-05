8804cb5751b3b55d5cabdb16c89db456980f7b42
Sofia <128095398+sofiarod2002@users.noreply.github.com>
2024-06-04 23:54:36 -0300

    Arregle buscar productos
    No se muestran productos que estén en intercambios aceptados o realizados

--- busqueda_de_productos/views.py
+++ busqueda_de_productos/views.py
@@ -6,7 +6,7 @@ def buscar_productos(request):
     usuario_actual = request.user
     if hasattr(usuario_actual, 'perfilcliente'):
         queryset = Producto.objects.exclude(cliente=usuario_actual)
-    else: 
+    else:
         queryset = Producto.objects.all()
 
     # Capturar el parámetro de búsqueda
@@ -26,6 +26,14 @@ def buscar_productos(request):
     sucursales = request.GET.getlist('sucursal')
     if sucursales:
         queryset = queryset.filter(sucursal__in=sucursales)
+
+   # Excluir productos en intercambios aceptados
+    intercambios_aceptados_o_realizados = Intercambio.objects.filter(estado__in=['aceptado', 'realizado']).values_list('producto_solicitante', 'producto_receptor')
+    productos_excluir = set()
+    for productos in intercambios_aceptados_o_realizados:
+        productos_excluir.update(productos)
+
+    queryset = queryset.exclude(id__in=productos_excluir)
 
     # Aplicar filtro de búsqueda
     if busqueda:

cfd88e9e1b1cf65536e6528b0c57b3bfcbf39d57
Sofia <sofiac2507@gmail.com>
2024-06-12 03:09:41 -0300

    hay servicios

--- gestion_de_servicios/models.py
+++ gestion_de_servicios/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.utils.text import slugify 
+from gestion_de_sucursales.models import Sucursal 
 
 class PagoServicio(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,3 +12,27 @@ class PagoServicio(models.Model):
 
     def __str__(self):
         return str(self.id) 
+    
+class Servicio(models.Model):
+    ESTADOS = (
+        ('pendiente', 'Pendiente'),   
+        ('aceptado', 'Aceptado'),
+        ('rechazado', 'Rechazado'),
+        ('publicado', 'Publicado'),
+    )
+    id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    estado = models.CharField(max_length=10, choices=ESTADOS, default='pendiente')
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)    
+    monto = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True) 
+    imagen = models.ImageField(upload_to='imagenes/')
+    slug = models.SlugField(null=True, blank=True)    
+    pago = models.ForeignKey(PagoServicio, on_delete=models.CASCADE, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.id) + "-" + str(self.imagen))
+        return super().save(*args , **kwargs)
+
+    def __str__(self):
+        return str(self.id) 
+    
